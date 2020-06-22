00320a82e08cedd09b1d48e3fc9f9998469c6142
Madhu5A3 <madhuv299@gmail.com>
2020-06-22 23:34:20 +0530

    Update: Variable Experimentation

--- programs/VariableExperimentation/variabledeclaration.cpp
+++ programs/VariableExperimentation/variabledeclaration.cpp
@@ -9,24 +9,23 @@ int main()
 
     //overwriting the previous value by assigning a different value to the same variable
     a = 2;
-    // cout << a << endl;
+    cout << a << endl;
 
     //double variable
     double b = 7.99;
-
-    // cout << b << endl;
+    cout << b << endl;
 
     //char variable
     char c = 'h';
-    // cout << c << endl;
+    cout << c << endl;
 
     //string variable
     string s = "world";
-    // cout << s << endl;
+    cout << s << endl;
 
     //bool variable
     bool bo = true;
-    // cout << bo;
+    cout << bo;
 
     // multiple printing with cout object using <<(insertion operator)
     cout << "Int Variable: " << a << endl
@@ -36,15 +35,14 @@ int main()
          << "Bool Variable: " << bo << endl;
 
     //adding variables together
-    int x= 5, y=6;
-    cout << "Sum of x and y: " << x+y;
+    int x = 5, y = 6;
+    cout << "Sum of x and y: " << x + y;
 
     // Ohh man!!! I dont like a variable being overrided that easy, let's give it some shield (read only)
 
     const int num = 9;
 
     // num = 10; //psst, the compiler will fuck you with warnings, saying you can't screw up variables like that
-
     cout << num;
     return 0;
 }
